f130f52aab6e574ac00d9a97c0cba831fa5c4532
Razvan Troaca <razvantr@yahoo.com>
2024-06-20 19:58:26 +0000

    Add toasts for adding and removing items to the shopping bag

--- bag/views.py
+++ bag/views.py
@@ -13,14 +13,18 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to the shopping bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -38,7 +42,7 @@ def adjust_bag(request, item_id):
             request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+        messages.success(request, f'Removed {product.name} from the shopping bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -52,7 +56,7 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+        messages.success(request, f'Removed {product.name} from the shopping bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
