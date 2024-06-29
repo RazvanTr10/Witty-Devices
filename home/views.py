509246bcf628811c54abad52e1d01d904e5957f2
Razvan Troaca <razvantr@yahoo.com>
2024-06-29 17:59:25 +0000

    Add views and url for privacy policy

--- home/views.py
+++ home/views.py
@@ -6,3 +6,10 @@ def index(request):
     """ A view to return to the index page """
 
     return render(request, 'home/index.html')
+
+def privacy_policy(request):
+    """
+    A view to render the privacy policy page
+    """
+    return render(request, "home/privacy_policy.html")
+
