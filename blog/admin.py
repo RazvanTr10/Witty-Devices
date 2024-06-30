b65d5f2e64589f59984f4973c6b38487e9cf1899
Razvan Troaca <razvantr@yahoo.com>
2024-06-30 10:44:15 +0000

    Add post_detail.html and urls, models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date')
+    list_filter = ('author', 'pub_date')
+    search_fields = ['title', 'content']
+    date_hierarchy = 'pub_date'
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('post', 'author', 'created_at')
+    search_fields = ['content']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
