057ba715e3cd8e1a974b89dedded651c11801f1c
Razvan Troaca <razvantr@yahoo.com>
2024-07-13 23:46:42 +0000

    Add admin.py update to newsletter app to change the django admin interface

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Subscriber
 
-# Register your models here.
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'date_added')
+    search_fields = ('email', 'name')
+    list_filter = ('date_added',)
+    ordering = ('-date_added',)
