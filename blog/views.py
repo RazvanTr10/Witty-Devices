cf6dfc535bcdf76012a7dc51e9aafee71aa12c27
Razvan Troaca <razvantr@yahoo.com>
2024-07-13 09:15:04 +0000

    Add post_list.html, changed the views, models and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import CommentForm
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-pub_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comments.all().order_by('-created_at')
+    
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid() and request.user.is_authenticated:
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.author = request.user
+            new_comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user == comment.author:
+        post_id = comment.post.id
+        comment.delete()
+    return redirect('post_detail', post_id=post_id)
